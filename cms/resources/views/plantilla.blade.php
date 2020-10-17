6a52eb4d1decefd3dde418dc8cb00c22facb4c7c
SS-Bulker <juanrincon@protonmail.com>
2020-10-17 11:51:39 -0500

    Arreglando la url del menu en la plantilla

--- cms/resources/views/plantilla.blade.php
+++ cms/resources/views/plantilla.blade.php
@@ -156,7 +156,7 @@
   <!-- Main Sidebar Container -->
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
-    <a href="" class="brand-link">
+    <a href="{{ url('/') }}" class="brand-link">
       <img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
            style="opacity: .8">
       <span class="brand-text font-weight-light">Menú </span>
