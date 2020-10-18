77d6c3032cc072c8a512c27f0030bfc262aa060d
SS-Bulker <juanrincon@protonmail.com>
2020-10-18 18:25:26 -0500

    Terminado de forma basica la vista de blog

--- cms/resources/views/paginas/blog.blade.php
+++ cms/resources/views/paginas/blog.blade.php
@@ -215,21 +215,26 @@
                           {{-- ROW VISUALIZAR REDES SOCIALES --}}
                           <div class="row">
 
-                            <div class="col-lg-6">
+                          @php
+                            $redes = json_decode($element->redes_sociales, true);
+
+                            foreach ($redes as $key => $value) {
+                              
+                              echo '<div class="col-lg-12">
 
                               <div class="input-group mb-3">
 
                                 <div class="input-group-prepend">
 
-                                  <div class="input-group-text text-white" style="background: #1475E0;">
+                                  <div class="input-group-text text-white" style="background:'.$value['background'].'">
 
-                                    <i class="fab fa-facebook-f"></i>
+                                    <i class="'.$value['icono'].'"></i>
 
                                   </div>
 
                                 </div>
 
-                                <input type="text" class="form-control" value="https://facebook.com/">
+                                <input type="text" class="form-control" value="'.$value['url'].'">
 
                                 <div class="input-group-prepend">
 
@@ -243,7 +248,10 @@
 
                               </div>
 
-                            </div>
+                            </div>';
+
+                            }
+                          @endphp
 
                           </div>
                           {{-- FIN ROW VISUALIZAR REDES SOCIALES --}}
@@ -261,6 +269,104 @@
                     
                     <div class="card-body">
                       
+                      <div class="row">
+
+                        <div class="col-lg-12">
+
+                          {{-- CAMBIAR LOGO --}}
+                          <div class="form-group my-2 text-center">
+
+                            <div class="btn btn-default btn-file mb-3">
+
+                              <i class="fas fa-paperclip"></i> Adjuntar imagen de logo
+
+                              <input type="file" class="form-control" name="logo">
+
+                            </div>
+
+                            <img src="{{url('/')}}/{{$element->logo}}" alt=""class="img-fluid py-2 bg-secondary">
+
+                            <p class="help-block small mt-3">Dimensiones: 700px * 200px | Peso maximo: 2mb | Fomrato: JPG o PNG</p>
+
+                          </div>
+
+                          <hr class="pd-2">
+
+                          {{-- CAMBIAR PORTADA --}}
+                          <div class="form-group my-2 text-center">
+
+                            <div class="btn btn-default btn-file mb-3">
+
+                              <i class="fas fa-paperclip"></i> Adjuntar imagen de portada
+
+                              <input type="file" class="form-control" name="portada">
+
+                            </div>
+
+                            <img src="{{url('/')}}/{{$element->portada}}" alt=""class="img-fluid py-2 bg-secondary">
+
+                            <p class="help-block small mt-3">Dimensiones: 700px * 420px | Peso maximo: 2mb | Fomrato: JPG o PNG</p>
+
+                          </div>
+
+                          <hr class="pd-2">
+
+                          {{-- CAMBIAR ICONO --}}
+                          <div class="form-group my-2 text-center">
+
+                            <div class="btn btn-default btn-file mb-3">
+
+                              <i class="fas fa-paperclip"></i> Adjuntar imagen de icono
+
+                              <input type="file" class="form-control" name="icono">
+
+                            </div>
+
+                            <br>
+
+                            <img src="{{url('/')}}/{{$element->icono}}" alt=""class="img-fluid py-2 rounded-circle">
+
+                            <p class="help-block small mt-3">Dimensiones: 150px * 150px | Peso maximo: 2mb | Fomrato: JPG o PNG</p>
+
+                          </div>
+
+                        </div>
+
+                      </div>
+
+                    </div>
+
+                  </div>
+
+                </div>
+
+                <div class="col-lg-6">
+
+                  <div class="card">
+                    
+                    <div class="card-body">
+
+                      <label for="">Sobre mi <span class="small">(Intro)</span></label>
+
+                    <textarea class="form-control" name="sobre_mi" id=""  rows="10">{{$element->sobre_mi}}</textarea>
+
+                    </div>
+
+                  </div>
+
+                </div>
+
+                <div class="col-lg-6">
+
+                  <div class="card">
+                    
+                    <div class="card-body">
+
+                      <label for="">Sobre mi completo <span class="small">(Completo)</span></label>
+
+
+                      <textarea class="form-control" name="sobre_mi_completo" id="" rows="10">{{$element->sobre_mi_completo}}</textarea>
+
                     </div>
 
                   </div>
